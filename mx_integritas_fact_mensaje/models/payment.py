1574c3987efbb4e8146af12eb02956c61bd86651
ColNacional <distribucion@colnal.mx>
2022-07-25 14:05:42 -0500

    demo

--- mx_integritas_fact_mensaje/models/payment.py
+++ mx_integritas_fact_mensaje/models/payment.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
+
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import _, fields, models
 import logging
 _logger = logging.getLogger(__name__)
 
-class PaymentTransaction(models.Model):
-    _inherit = "payment.transaction"
 
-    is_donation = fields.Boolean(string="Is donation", help="Is the payment a donation")
+class PaymentTransaction(models.Model):
+
+    _inherit = 'payment.transaction'
 
     def _finalize_post_processing(self):
-	try:
-        	super()._finalize_post_processing()
-	except Exception as e:
-		_logger.exception(e)
+        try:
+            super()._finalize_post_processing()
+        except Exception, e:
+            _logger.exception(e)
